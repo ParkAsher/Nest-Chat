cf6da0d15b2c035aff58f7297b0c20fb4d479874
ParkAsher <devasherpark@naver.com>
2023-09-27 14:42:09 +0900

    게이트웨이 닉네임 수정

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -14,10 +14,11 @@ export class ChatGateway {
     // message 이벤트 구독
     @SubscribeMessage('message')
     handleMessage(socket: Socket, data: any): void {
-        // 접속한 클라이언트 들에 메시지 전송
-        this.server.emit(
-            'message',
-            `client-${socket.id.substring(0, 4)} : ${data}`,
-        );
+        // 메시지와 닉네임을 데이터에서 추출
+        const { message, nickname } = data;
+
+        // 닉네임을 포함한 메시지 전송
+        // socket.broadcast.emit() 은 전송을 요청한 클라이언트를 제외한 다른 클라이언트들에게 데이터 전송
+        socket.broadcast.emit('message', `${nickname}: ${message}`);
     }
 }
