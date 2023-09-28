679a7243c15e43fbab5bd1e59fb5721d6f2f2911
ParkAsher <devasherpark@naver.com>
2023-09-28 15:45:47 +0900

    방 생성 공지 이벤트와 방 입장 핸들러 구현

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -26,6 +26,8 @@ export class ChatGateway {
 
 @WebSocketGateway({ namespace: 'room' })
 export class RoomGateway {
+    // 채팅 게이트웨이 의존성 주입
+    constructor(private readonly chatGateway: ChatGateway) {}
     rooms = [];
 
     // 서버 인스턴스 접근을 위한 변수 생성
@@ -36,9 +38,30 @@ export class RoomGateway {
     @SubscribeMessage('createRoom')
     handleMessage(@MessageBody() data) {
         const { room, nickname } = data;
+
+        // 채팅방 생성 시 이벤트 발생시켜 클라이언트에 송신
+        this.chatGateway.server.emit('notice', {
+            message: `${nickname}님이 ${room}방을 만들었습니다.`,
+        });
+
         // 채팅방 이름 받아서 리스트에 추가
         this.rooms.push(room);
         // rooms 이벤트로 채팅방 리스트 전송
         this.server.emit('rooms', this.rooms);
+    }
+
+    // 채팅방 입장 시 실행되는 핸들러 메서드
+    @SubscribeMessage('joinRoom')
+    handleJoinRoom(socket: Socket, data) {
+        const { nickname, room, toLeaveRoom } = data;
+
+        // 기존에 방에서 먼저나감
+        socket.leave(toLeaveRoom);
+        this.chatGateway.server.emit('notice', {
+            message: `${nickname}님이 ${room}방에 입장했습니다.`,
+        });
+
+        // 새로운 방에 입장
+        socket.join(room);
     }
 }
