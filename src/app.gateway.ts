5a5d19214f5d4646d046daf5d28c5b5a787d2ad9
ParkAsher <devasherpark@naver.com>
2023-09-28 14:34:12 +0900

    채팅방 생성 기능 및 채팅방 리스트 전송 기능 구현

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -2,6 +2,7 @@ import {
     SubscribeMessage,
     WebSocketGateway,
     WebSocketServer,
+    MessageBody,
 } from '@nestjs/websockets';
 
 import { Server, Socket } from 'socket.io';
@@ -20,5 +21,24 @@ export class ChatGateway {
         // 닉네임을 포함한 메시지 전송
         // socket.broadcast.emit() 은 전송을 요청한 클라이언트를 제외한 다른 클라이언트들에게 데이터 전송
         socket.broadcast.emit('message', `${nickname}: ${message}`);
+    }
+}
+
+@WebSocketGateway({ namespace: 'room' })
+export class RoomGateway {
+    rooms = [];
+
+    // 서버 인스턴스 접근을 위한 변수 생성
+    @WebSocketServer() server: Server;
+
+    // createRoom 핸들러 메서드
+    // 소켓없이 데이터만 받음
+    @SubscribeMessage('createRoom')
+    handleMessage(@MessageBody() data) {
+        const { room, nickname } = data;
+        // 채팅방 이름 받아서 리스트에 추가
+        this.rooms.push(room);
+        // rooms 이벤트로 채팅방 리스트 전송
+        this.server.emit('rooms', this.rooms);
     }
 }
